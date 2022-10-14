26a25750a3807195bcf1b1c61d056b95abe410dd
Happyfew <459060865@qq.com>
2022-10-14 17:28:14 +0800

    feat: 不生效，再试试

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-    ...require('prettier-airbnb-config'),
+    // ...require('prettier-airbnb-config'),
+    tabWidth: 2, // 缩进4个字符
+    printWidth: 80, //超过多少换行
+    singleQuote: true, //字符串是否使用单引号，默认为false，使用双引号
+    semi: true, //行位是否使用分号，默认为true
+    trailingComma: 'all', //是否使用尾逗号，有三个可选值"<none|es5|all>"
+    arrowParens: 'avoid', // x => x
+    jsxBracketSameLine: true,
+    requirePragma: false, //顶部注释
+    htmlWhitespaceSensitivity: 'ignore',
 }
