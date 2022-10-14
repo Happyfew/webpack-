3cf1b3aaf291e9b87d8490b20ffc2b816129e1b9
Happyfew <459060865@qq.com>
2022-10-14 17:33:51 +0800

    再试试吧

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './app.less';
 
 const App = () => {
-    const userId   =    'test Prettier';
+  const userId = 'test Prettier';
 
-    return (
-        <>
-                <div className="box">
-                    <div className="box1">my-project!!!----1</div>
-                <div className="box2">my-project!!!----</div>
-            </div>
-        </>    
-    )
-}
+  return (
+    <>
+      <div className="box">
+        <div className="box1">my-project!!!----1</div>
+        <div className="box2">my-project!!!----</div>
+      </div>
+    </>
+  );
+};
 const container = document.getElementById('root')!;
 const root = ReactDOM.createRoot(container);
-root.render(<App/>)
+root.render(<App />);
